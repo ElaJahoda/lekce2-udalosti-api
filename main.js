68261e1a8f44cef911960e7e2ffee5ffc7fdaee3
Ela Jahoda <3la.jah0da@gmail.com>
2022-02-18 00:20:54 +0100

    [0] change text to bold if mouseover

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 // tady je místo pro náš program
+let tucny = document.querySelector('p');
+
+function mouseOver() {
+    tucny.classList.add('tucny');
+}
+
+function mouseOut() {
+    tucny.classList.remove('tucny');
+}

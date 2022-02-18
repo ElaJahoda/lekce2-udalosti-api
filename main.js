37ed92a85b70c069e214aab4afa666ade69c0a57
Ela Jahoda <3la.jah0da@gmail.com>
2022-02-18 13:32:29 +0100

    [1] red paragraph after click - toggle

--- main.js
+++ main.js
@@ -1,10 +1,24 @@
-// tady je místo pro náš program
-let tucny = document.querySelector('p');
+// Úkol: Písmo v odstavci po najetí myši ztuční.
+let odstavec = document.querySelector('p');
 
 function mouseOver() {
-    tucny.classList.add('tucny');
+    odstavec.classList.add('tucny');
 }
 
 function mouseOut() {
-    tucny.classList.remove('tucny');
+    odstavec.classList.remove('tucny');
 }
+
+// Úkol 1: Tlačítko “červené po kliknutí” bude odstavci přidávat/odebírat 
+//třídu, která bude barvu písma v odstavci měnit na červenou.
+
+function cervenyPoKliknuti() {
+    odstavec.classList.toggle('cerveny')
+}
+
+// Úkol 2: přidej tlačítko “větší po kliknutí”, které písmo zvětší 
+// o jeden pixel oproti jeho aktuální velikost.
+
+
+// Úkol 3: Dopracuj ovládání audio souboru:
+// pauznutí zvukové stopy, nastavení zvuku na 0, 0.5 a 1, vrácení se na začátek
